ea64f733467ef9ddd9c8c68388250c665fc09808
Stevendotcom <nico.gallardo.8100@outlook.com>
2024-11-01 13:01:59 -0300

    add signatures and implement init

---
+++ Flok/src/actors/PlayerClass.cpp
@@ -0,0 +1,15 @@
+﻿#include "PlayerClass.h"
+
+#include "Constants.h"
+#include "engine/Math.h"
+
+namespace {
+PlayerClass::PlayerType Player;
+
+}
+
+void PlayerClass::Init() {
+  Player.f_BoundingBox = {51, g_ScreenHeight - 101, 100, 100};
+  Player.f_Direction = {1, 0};
+}
+
---
+++ Flok/src/actors/PlayerClass.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <raylib.h>
+
+namespace PlayerClass {
+struct PlayerType {
+  Rectangle f_BoundingBox;
+  Vector2 f_Direction;
+  float f_Speed;
+  //Texture2D f_Sprite;
+};
+
+void Init();
+void Push(const Vector2& Direction, float Force);
+void Update();
+void Draw();
+
+};
