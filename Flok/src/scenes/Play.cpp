1bdeba5f6d2180593dfbf14acff60bce140ee30f
Stevendotcom <nico.gallardo.8100@outlook.com>
2024-11-01 13:00:20 -0300

    implement basic struct of play

---
+++ Flok/src/scenes/Play.cpp
@@ -0,0 +1,32 @@
+﻿#include "Play.h"
+
+#include "raylib.h"
+#include "actors/PlayerClass.h"
+
+namespace  {
+
+bool Exit = false;
+constexpr float k_PlayerUpwardPush = 300.0F;
+
+void Input() {
+  
+}
+
+void Update() {
+  
+}
+
+void Draw() {
+  
+}
+
+}
+
+void Play::Play() {
+  PlayerClass::Init();
+  while(!Exit && !WindowShouldClose()) {
+    Input();
+    Update();
+    Draw();
+  }
+}
