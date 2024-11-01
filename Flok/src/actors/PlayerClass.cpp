f8cf3b9d5c80d33524c7d5596359e48a63d0424d
Stevendotcom <nico.gallardo.8100@outlook.com>
2024-11-01 13:02:26 -0300

    implement all player functions

--- Flok/src/actors/PlayerClass.cpp
+++ Flok/src/actors/PlayerClass.cpp
@@ -13,3 +13,29 @@ void PlayerClass::Init() {
   Player.f_Direction = {1, 0};
 }
 
+void PlayerClass::Push(const Vector2& Direction, const float Force) {
+  Vector2 Temp = Math::Multiply(Direction, Force * GetFrameTime());
+  Temp = Math::Add(Player.f_Direction, Temp);
+  Player.f_Direction = Math::Normalize(Temp, Math::GetMag(Temp));
+  Player.f_Speed += Force * GetFrameTime();
+}
+
+void PlayerClass::Update() {
+  //add gravity
+  if (Player.f_BoundingBox.y > Player.f_BoundingBox.height / 2.0F && Player.
+      f_BoundingBox.y < g_ScreenHeight - Player.f_BoundingBox.height / 2.0F) {
+    Player.f_BoundingBox.y += Player.f_Direction.y * Player.f_Speed;
+      } else if (Math::IsEqual(
+          Player.f_BoundingBox.y + Player.f_BoundingBox.height / 2.0F,
+          g_ScreenHeight)) {
+        Player.f_Speed = 0;
+        Player.f_Direction = {1, 0};
+          }
+}
+
+void PlayerClass::Draw() {
+  DrawRectanglePro(Player.f_BoundingBox,
+                   {Player.f_BoundingBox.width / 2.0F,
+                    Player.f_BoundingBox.height / 2.0F},
+                   Math::GetRotation(Player.f_Direction), GREEN);
+}
