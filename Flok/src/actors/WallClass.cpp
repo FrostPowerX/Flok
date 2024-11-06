d0f044f3572bda43d3686a59080f8070ac8205b2
Alex Gallardo <gallardonicolas.8100@outlook.com>
2024-11-06 20:17:21 -0300

    implement a wall

---
+++ Flok/src/actors/WallClass.cpp
@@ -0,0 +1,69 @@
+﻿#include "WallClass.h"
+
+#include "Constants.h"
+
+namespace {
+
+constexpr float k_WallWidth = 100;
+constexpr float k_MaxGapH = 300;
+constexpr float k_MinGapH = 50;
+
+int UniqueId = 0;
+
+
+
+float GetRandomGapStart() {
+  return static_cast<float>(GetRandomValue(0, g_ScreenHeight));
+}
+
+
+
+float GetRandomGapHeight() {
+  return static_cast<float>(GetRandomValue(static_cast<int>(k_MinGapH),
+                                           static_cast<int>(k_MaxGapH)));
+}
+
+};
+
+
+
+Wall::WallType Wall::Make(float Speed) {
+
+  return {.f_Position = g_ScreenWidth,
+          .f_GapStart = GetRandomGapStart(),
+          .f_GapSize = GetRandomGapHeight(),
+          .f_Speed = Speed,
+          .f_IsHidden = true,
+          .f_UniqueId = UniqueId++};
+}
+
+
+
+void Wall::Reset(WallType& Wall, const float Speed) {
+  Wall = {.f_Position = g_ScreenWidth,
+          .f_GapStart = GetRandomGapStart(),
+          .f_GapSize = GetRandomGapHeight(),
+          .f_Speed = Speed,
+          .f_IsHidden = true,
+          .f_UniqueId = Wall.f_UniqueId};
+}
+
+
+
+void Wall::Update(WallType& Wall) {
+  Wall.f_Position -= Wall.f_Speed * GetFrameTime();
+}
+
+
+
+void Wall::Draw(const WallType& Wall) {
+  //Change to draw Texture
+  DrawRectangle(static_cast<int>(Wall.f_Position), 0,
+                static_cast<int>(k_WallWidth),
+                static_cast<int>(Wall.f_GapStart), GREEN);
+
+  DrawRectangle(static_cast<int>(Wall.f_Position),
+                static_cast<int>(Wall.f_GapStart + Wall.f_GapSize),
+                static_cast<int>(k_WallWidth), g_ScreenHeight, GREEN);
+
+}
---
+++ Flok/src/actors/WallClass.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "raylib.h"
+
+namespace Wall {
+struct WallType {
+  float f_Position;
+  float f_GapStart;
+  float f_GapSize;
+  float f_Speed;
+  bool f_IsHidden;
+  int f_UniqueId;
+};
+
+WallType Make(float Speed);
+
+void Reset(WallType Wall&Wall, float Speed);
+
+void Update(WallType Wall&Wall);
+
+void Draw(const WallType Wall&Wall);
+};
