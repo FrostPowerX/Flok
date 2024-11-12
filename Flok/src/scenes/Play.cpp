f8ab9501e5d612e3601fe3c5cb9a914d76db0102
FrostPowerX <103634826+FrostPowerX@users.noreply.github.com>
2024-11-12 12:53:06 -0300

    Add pause Menu

--- Flok/src/scenes/Play.cpp
+++ Flok/src/scenes/Play.cpp
@@ -8,15 +8,25 @@
 #include "Constants.h"
 #include "actors/PlayerClass.h"
 #include "actors/WallClass.h"
+#include "engine/Buttons.h"
 #include "engine/Collisions.h"
 #include "engine/SceneManager.h"
 
 namespace {
 
+using namespace Buttons;
+
+const int maxButtons = 2;
+
 Texture2D backGround;
 Texture2D midGround;
 Texture2D midGround2;
 Texture2D frontGround;
+
+Button pauseMenu[maxButtons];
+
+constexpr int fontSize = 32;
+int hover = 1;
 
 float scrollingBack = 0.0f;
 float scrollingMid = 0.0f;
@@ -28,6 +38,8 @@ constexpr float k_PlayerUpwardPush = 5.0F;
 constexpr float k_WallTimer = 3.0F;
 constexpr float k_WallSpeed = 400.0F;
 float WallTime = 0;
+
+bool pause = false;
 
 void WallManager(std::list<Wall::WallType*>& Walls, std::stack<Wall::WallType*>& HiddenWalls) {
   // TODO
@@ -71,6 +83,8 @@ void WallManager(std::list<Wall::WallType*>& Walls, std::stack<Wall::WallType*>&
 }
 
 void Init() {
+  hover = 1;
+
   backGround = LoadTexture("res/JungleTileset/ParallaxBackground/Back.png");
   midGround = LoadTexture("res/JungleTileset/ParallaxBackground/Back2.png");
   midGround2 = LoadTexture("res/JungleTileset/ParallaxBackground/Back3.png");
@@ -87,12 +101,71 @@ void Init() {
 
   frontGround.height = g_ScreenHeight;
   frontGround.width = g_ScreenWidth;
+
+  Texture2D k_Def = {.id = 0, .width = 0, .height = 0, .mipmaps = 0, .format = 0};
+  Font k_DefFont = {
+      .baseSize = 0, .glyphCount = 0, .glyphPadding = 0, .texture = k_Def, .recs = nullptr, .glyphs = nullptr};
+
+  Margin k_Margin = {.f_Top = 40, .f_Bottom = 0, .f_Left = 0, .f_Right = 0};
+  Padding k_Padding = {.f_Top = 10, .f_Bottom = 0, .f_Left = 0, .f_Right = 0};
+
+  std::string text;
+
+  for (int I = 0; I < maxButtons; I++) {
+
+    switch (I) {
+      case 0:
+        text = "Continue";
+        break;
+      case 1:
+        text = "Menu";
+    }
+
+    pauseMenu[I] = {
+        .f_BoundingBox = {.x = (g_ScreenWidth - static_cast<float>(MeasureText(text.c_str(), fontSize))) / 2.0F,
+                          .y = g_ScreenHeight / 2.0F + k_Margin.f_Top * static_cast<float>(I + 1),
+                          .width = static_cast<float>(MeasureText(text.c_str(), fontSize)) + k_Padding.f_Top,
+                          .height = fontSize},
+        .f_Padding = k_Padding,
+        .f_Margin = k_Padding,
+        .f_Sprite = k_Def,
+        .f_HoverTint = WHITE,
+        .f_TextColor = WHITE,
+        .f_Text = text,
+        .f_Font = k_DefFont,
+        .f_FontSize = fontSize,
+        .f_IsHover = false,
+        .f_IsTextCenter = true};
+  }
 }
 
 void Input() {
-  if (IsKeyPressed(KEY_SPACE)) {
-    // PlayerClass::Push({0, -1}, k_PlayerUpwardPush + PlayerClass::GetSpeed());
-    PlayerClass::Jump(k_PlayerUpwardPush);
+
+  if (IsKeyPressed(KEY_ESCAPE))
+    pause = !pause;
+
+  if (pause) {
+    Buttons::Input(pauseMenu, hover, maxButtons);
+
+    if (IsKeyReleased(KEY_ENTER)) {
+      switch (hover) {
+
+        case 1:
+          pause = !pause;
+          break;
+
+        case 2:
+          Exit = true;
+          pause = !pause;
+          break;
+      }
+    }
+
+  } else {
+    if (IsKeyPressed(KEY_SPACE)) {
+      // PlayerClass::Push({0, -1}, k_PlayerUpwardPush + PlayerClass::GetSpeed());
+      PlayerClass::Jump(k_PlayerUpwardPush);
+    }
   }
 }
 
@@ -114,38 +187,41 @@ bool CheckForPlayerWallCollision(const std::list<Wall::WallType*>& Walls) {
 
 void Update(std::list<Wall::WallType*>& Walls, std::stack<Wall::WallType*>& HiddenWalls) {
 
-  bool HitsBorder = false;
-  bool HitsWall = false;
+  if (!pause) {
 
-  scrollingBack -= 0.0f;
-  scrollingMid -= k_WallSpeed * 0.25f * GetFrameTime();
-  scrollingMid2 -= k_WallSpeed * 0.5f * GetFrameTime();
-  scrollingFore -= k_WallSpeed * GetFrameTime();
+    bool HitsBorder = false;
+    bool HitsWall = false;
 
-  if (scrollingBack <= -backGround.width)
-    scrollingBack = 0;
-  if (scrollingMid <= -midGround.width)
-    scrollingMid = 0;
-  if (scrollingMid2 <= -midGround2.width)
-    scrollingMid2 = 0;
-  if (scrollingFore <= -frontGround.width)
-    scrollingFore = 0;
+    scrollingBack -= 0.0f;
+    scrollingMid -= k_WallSpeed * 0.25f * GetFrameTime();
+    scrollingMid2 -= k_WallSpeed * 0.5f * GetFrameTime();
+    scrollingFore -= k_WallSpeed * GetFrameTime();
 
-  PlayerClass::Update();
-  WallManager(Walls, HiddenWalls);
+    if (scrollingBack <= -backGround.width)
+      scrollingBack = 0;
+    if (scrollingMid <= -midGround.width)
+      scrollingMid = 0;
+    if (scrollingMid2 <= -midGround2.width)
+      scrollingMid2 = 0;
+    if (scrollingFore <= -frontGround.width)
+      scrollingFore = 0;
 
-  HitsWall = CheckForPlayerWallCollision(Walls);
-  HitsBorder = Collisions::IsRectBorder(PlayerClass::GetBoundingBox());
+    PlayerClass::Update();
+    WallManager(Walls, HiddenWalls);
 
-  // in case it hits roof
-  if (HitsBorder) {
-    if (PlayerClass::GetBoundingBox().y < PlayerClass::GetBoundingBox().height) {
-      PlayerClass::MovePlayer(10);
-    } else {
+    HitsWall = CheckForPlayerWallCollision(Walls);
+    HitsBorder = Collisions::IsRectBorder(PlayerClass::GetBoundingBox());
+
+    // in case it hits roof
+    if (HitsBorder) {
+      if (PlayerClass::GetBoundingBox().y < PlayerClass::GetBoundingBox().height) {
+        PlayerClass::MovePlayer(10);
+      } else {
+        Exit = true;
+      }
+    } else if (HitsWall) {
       Exit = true;
     }
-  } else if (HitsWall) {
-    Exit = true;
   }
 }
 
@@ -174,6 +250,10 @@ void Draw(const std::list<Wall::WallType*>& Walls) {
     DrawTextureEx(frontGround, Vector2(scrollingFore, 0), 0.0f, 1.0f, WHITE);
     DrawTextureEx(frontGround, Vector2(frontGround.width + scrollingFore, 0), 0.0f, 1.0f, WHITE);
   }
+
+  if (pause)
+    RenderButtons(pauseMenu, maxButtons);
+
   EndDrawing();
 }
 
