f59a3c30ea0676568482dd3f37f3252c5ca16cec
FrostPowerX <103634826+FrostPowerX@users.noreply.github.com>
2024-11-12 10:18:20 -0300

    Fixed Player Colision

--- Flok/src/engine/Collisions.cpp
+++ Flok/src/engine/Collisions.cpp
@@ -1,25 +1,23 @@
 ﻿#include "Collisions.h"
 
+#include <iostream>
+
 #include "raylib.h"
 
 #include "Constants.h"
-
-
 
 bool Collisions::IsCircleCircle(const Vector2& APosition,
                                 const float ARadius,
                                 const Vector2& BPosition,
                                 const float BRadius) {
-  const float k_Dist = (APosition.x - BPosition.x) * (APosition.x - BPosition.x) + (APosition.y - BPosition.y) * (
-                         APosition.y - BPosition.y);
+  const float k_Dist = (APosition.x - BPosition.x) * (APosition.x - BPosition.x) +
+                       (APosition.y - BPosition.y) * (APosition.y - BPosition.y);
   const float k_Radii = (ARadius + BRadius) * (ARadius + BRadius);
 
   // Sum(Radius)^2 >= Dist^2
   // saves the necessity of a sqrt
   return k_Radii >= k_Dist;
 }
-
-
 
 bool Collisions::IsBorderCircle(const Vector2& Position, const float Radius, WhereCollides& CollisionPlace) {
 
@@ -44,8 +42,6 @@ bool Collisions::IsBorderCircle(const Vector2& Position, const float Radius, Whe
   return Collides;
 }
 
-
-
 bool Collisions::IsDotBorder(const Vector2& Position, WhereCollides& CollisionPlace) {
   bool Collides = false;
 
@@ -64,28 +60,35 @@ bool Collisions::IsDotBorder(const Vector2& Position, WhereCollides& CollisionPl
   }
 
   return Collides;
-
 }
-
-
 
 bool Collisions::IsRectRect(const Rectangle& A, const Rectangle& B) {
-  if ((A.x < B.x && A.x + A.width > B.x) || (A.x > B.x && A.x < B.x + B.width)) {
-    if ((A.y < B.y && A.y + A.height > B.y) || (A.y > B.y && A.y < B.y + B.width)) {
-      return true;
-    }
-  }
-  return false;
+
+  float halfWA = A.width / 2.f;
+  float halfHA = A.height / 2.f;
+
+  float halfWB = B.width / 2.f;
+  float halfHB = B.height / 2.f;
+
+  float centerAX = A.x + halfWA;
+  float centerAY = A.y + halfHA;
+
+  float centerBX = B.x + halfWB;
+  float centerBY = B.y + halfHB;
+
+  float distanceX = abs(centerAX - centerBX);
+  float distanceY = abs(centerAY - centerBY);
+
+  distanceX -= halfWA + halfWB;
+  distanceY -= halfHA + halfHB;
+
+  return (distanceX <= 0 && distanceY <= 0);
 }
-
-
 
 bool Collisions::IsRectBorder(const Rectangle& A) {
   //  REMOVE
   //@formatter:off
-  return A.x <= 0 ||
-         A.x + A.width >= static_cast<float>(g_ScreenWidth) ||
-         A.y <= 0 ||
+  return A.x <= 0 || A.x + A.width >= static_cast<float>(g_ScreenWidth) || A.y <= 0 ||
          A.y + A.height >= static_cast<float>(g_ScreenHeight);
   //@formatter:on
 }
