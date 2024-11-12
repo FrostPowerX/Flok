4c7b4acd4ebcf469a647b72ec25daafc962c430a
FrostPowerX <103634826+FrostPowerX@users.noreply.github.com>
2024-11-12 12:18:59 -0300

    Add Parallax

--- Flok/src/scenes/Play.cpp
+++ Flok/src/scenes/Play.cpp
@@ -13,10 +13,20 @@
 
 namespace {
 
+Texture2D backGround;
+Texture2D midGround;
+Texture2D midGround2;
+Texture2D frontGround;
+
+float scrollingBack = 0.0f;
+float scrollingMid = 0.0f;
+float scrollingMid2 = 0.0f;
+float scrollingFore = 0.0f;
+
 bool Exit = false;
 constexpr float k_PlayerUpwardPush = 5.0F;
 constexpr float k_WallTimer = 3.0F;
-constexpr float k_WallSpeed = 800.0F;
+constexpr float k_WallSpeed = 400.0F;
 float WallTime = 0;
 
 void WallManager(std::list<Wall::WallType*>& Walls, std::stack<Wall::WallType*>& HiddenWalls) {
@@ -60,9 +70,28 @@ void WallManager(std::list<Wall::WallType*>& Walls, std::stack<Wall::WallType*>&
   }
 }
 
+void Init() {
+  backGround = LoadTexture("res/JungleTileset/ParallaxBackground/Back.png");
+  midGround = LoadTexture("res/JungleTileset/ParallaxBackground/Back2.png");
+  midGround2 = LoadTexture("res/JungleTileset/ParallaxBackground/Back3.png");
+  frontGround = LoadTexture("res/JungleTileset/ParallaxBackground/BushFront.png");
+
+  backGround.height = g_ScreenHeight;
+  backGround.width = g_ScreenWidth;
+
+  midGround.height = g_ScreenHeight;
+  midGround.width = g_ScreenWidth;
+
+  midGround2.height = g_ScreenHeight;
+  midGround2.width = g_ScreenWidth;
+
+  frontGround.height = g_ScreenHeight;
+  frontGround.width = g_ScreenWidth;
+}
+
 void Input() {
   if (IsKeyPressed(KEY_SPACE)) {
-    //PlayerClass::Push({0, -1}, k_PlayerUpwardPush + PlayerClass::GetSpeed());
+    // PlayerClass::Push({0, -1}, k_PlayerUpwardPush + PlayerClass::GetSpeed());
     PlayerClass::Jump(k_PlayerUpwardPush);
   }
 }
@@ -88,6 +117,20 @@ void Update(std::list<Wall::WallType*>& Walls, std::stack<Wall::WallType*>& Hidd
   bool HitsBorder = false;
   bool HitsWall = false;
 
+  scrollingBack -= 0.0f;
+  scrollingMid -= k_WallSpeed * 0.25f * GetFrameTime();
+  scrollingMid2 -= k_WallSpeed * 0.5f * GetFrameTime();
+  scrollingFore -= k_WallSpeed * GetFrameTime();
+
+  if (scrollingBack <= -backGround.width)
+    scrollingBack = 0;
+  if (scrollingMid <= -midGround.width)
+    scrollingMid = 0;
+  if (scrollingMid2 <= -midGround2.width)
+    scrollingMid2 = 0;
+  if (scrollingFore <= -frontGround.width)
+    scrollingFore = 0;
+
   PlayerClass::Update();
   WallManager(Walls, HiddenWalls);
 
@@ -107,6 +150,15 @@ void Update(std::list<Wall::WallType*>& Walls, std::stack<Wall::WallType*>& Hidd
 }
 
 void Draw(const std::list<Wall::WallType*>& Walls) {
+  DrawTextureEx(backGround, Vector2(scrollingBack, 0), 0.0f, 1.0f, WHITE);
+  DrawTextureEx(backGround, Vector2(backGround.width + scrollingBack, 0), 0.0f, 1.0f, WHITE);
+
+  DrawTextureEx(midGround, Vector2(scrollingMid, 0), 0.0f, 1.0f, WHITE);
+  DrawTextureEx(midGround, Vector2(midGround.width + scrollingMid, 0), 0.0f, 1.0f, WHITE);
+
+  DrawTextureEx(midGround2, Vector2(scrollingMid2, 0), 0.0f, 1.0f, WHITE);
+  DrawTextureEx(midGround2, Vector2{midGround2.width + scrollingMid2, 0}, 0.0f, 1.0f, WHITE);
+
   BeginDrawing();
   {
     ClearBackground(RAYWHITE);
@@ -118,11 +170,21 @@ void Draw(const std::list<Wall::WallType*>& Walls) {
     }
 
     PlayerClass::Draw();
+
+    DrawTextureEx(frontGround, Vector2(scrollingFore, 0), 0.0f, 1.0f, WHITE);
+    DrawTextureEx(frontGround, Vector2(frontGround.width + scrollingFore, 0), 0.0f, 1.0f, WHITE);
   }
   EndDrawing();
 }
 
 } // namespace
+
+void DeInit() {
+  UnloadTexture(backGround);
+  UnloadTexture(midGround);
+  UnloadTexture(midGround2);
+  UnloadTexture(frontGround);
+}
 
 void Play::Play() {
   // BUG:
@@ -131,6 +193,7 @@ void Play::Play() {
   std::list<Wall::WallType*> Walls;
   std::stack<Wall::WallType*> HiddenWalls;
 
+  Init();
   PlayerClass::Init();
 
   while (!Exit && !WindowShouldClose()) {
@@ -146,6 +209,8 @@ void Play::Play() {
     delete HiddenWalls.top();
     HiddenWalls.pop();
   }
+
+  DeInit();
 
   Exit = false;
   PlayerClass::Unload();
