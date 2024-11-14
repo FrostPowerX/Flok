14e0e05c0f326d88ff8aadfde9fe83f829e5af3c
FrostPowerX <103634826+FrostPowerX@users.noreply.github.com>
2024-11-14 18:15:11 -0300

    Add Player 2

---
+++ Flok/src/actors/PlayerClass2.cpp
@@ -0,0 +1,95 @@
+﻿#include "PlayerClass2.h"
+
+#include "raylib.h"
+
+#include "Constants.h"
+#include "engine/Collisions.h"
+#include "engine/Math.h"
+
+namespace {
+
+constexpr float k_SpeedLimit = 50000.0F;
+constexpr float k_FramesHop = 7.0F;
+constexpr int k_Scale = 2;
+
+PlayerClass2::PlayerType Player;
+} // namespace
+
+void PlayerClass2::Init() {
+  Player.f_BoundingBox = {.x = 51.0F, .y = g_ScreenHeight / 2.0F, .width = 48.0F, .height = 48.0F};
+  Player.f_Direction = {.x = 1, .y = 0};
+  Player.f_Sprite = LoadTexture("res/ToxicFrog/BlueBlue/ToxicFrogBlueBlue_Hop.png");
+  Player.f_FrameTimer = 0;
+  Player.f_Speed = 0;
+}
+
+void PlayerClass2::Unload() {
+  if (IsTextureReady(Player.f_Sprite)) {
+    UnloadTexture(Player.f_Sprite);
+  }
+}
+
+void PlayerClass2::Push(const Vector2& Direction, float Force) {
+
+  Vector2 Temp = Math::Multiply(Direction, Force * GetFrameTime());
+  Temp = Math::Add(Player.f_Direction, Temp);
+
+  // TODO
+  //  Fix rotation so if it is aiming down, it remains looking forward
+  //  Maybe use Player.f_Direction.x *= -1 if y is positive
+  Player.f_Direction = Math::Normalize(Temp, Math::GetMag(Temp));
+
+  if (Player.f_Speed < k_SpeedLimit && Direction.y > 0) {
+    Player.f_Speed += Player.f_Direction.y + Force * GetFrameTime();
+  } else if (Player.f_Speed < k_SpeedLimit) {
+    Player.f_Speed += Player.f_Direction.y + (-Force);
+  } else {
+    Player.f_Speed = k_SpeedLimit;
+  }
+}
+
+void PlayerClass2::Update() {
+  Push({.x = 0, .y = 1}, g_Gravity);
+
+  if (!Collisions::IsRectBorder(Player.f_BoundingBox)) {
+    Player.f_BoundingBox.y += Player.f_Direction.y * Player.f_Speed * GetFrameTime();
+  }
+
+  Player.f_FrameTimer += GetFrameTime();
+  if (Player.f_FrameTimer >= g_Second / k_FramesHop) {
+    Player.f_FrameTimer = 0;
+    Player.f_Frame++;
+
+    if (Player.f_Frame > k_FramesHop) {
+      Player.f_Frame = 0;
+    }
+  }
+}
+
+void PlayerClass2::Draw() {
+  DrawRectangleLinesEx(Player.f_BoundingBox, 2, GREEN);
+  DrawTexturePro(Player.f_Sprite, {.x = 48 * (Player.f_Frame * 2 + 1), .y = 8, .width = 48, .height = 32},
+                 {.x = Player.f_BoundingBox.x + Player.f_BoundingBox.width / 2.0F,
+                  .y = Player.f_BoundingBox.y + Player.f_BoundingBox.height / 2.0F,
+                  .width = Player.f_BoundingBox.width * k_Scale,
+                  .height = Player.f_BoundingBox.height * k_Scale},
+                 {.x = Player.f_BoundingBox.width * k_Scale / 2.0F, .y = Player.f_BoundingBox.height * k_Scale / 2.0F},
+                 0.0F, BLUE);
+}
+
+Rectangle PlayerClass2::GetBoundingBox() {
+  return Player.f_BoundingBox;
+}
+
+void PlayerClass2::SetPosition(const int x, const int y) {
+  Player.f_BoundingBox.x = x;
+  Player.f_BoundingBox.y = y;
+}
+
+void PlayerClass2::MovePlayer(const float Height) {
+  Player.f_BoundingBox.y += Height;
+}
+
+float PlayerClass2::GetSpeed() {
+  return Player.f_Speed;
+}
---
+++ Flok/src/actors/PlayerClass2.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include <raylib.h>
+
+namespace PlayerClass2 {
+struct PlayerType {
+  Rectangle f_BoundingBox;
+  Vector2 f_Direction;
+  float f_Speed;
+  float f_FrameTimer;
+  float f_Frame;
+  Texture2D f_Sprite;
+};
+
+
+
+void Init();
+
+void Unload();
+
+void Push(const Vector2& Direction, float Force);
+
+void Update();
+
+void Draw();
+
+Rectangle GetBoundingBox();
+
+void SetPosition(int x, int y);
+
+void MovePlayer(float Height);
+
+float GetSpeed();
+};
