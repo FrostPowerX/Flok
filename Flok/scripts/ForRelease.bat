26632b5e5bee6c1636529f01f0ba8c392bb6b520
FrostPowerX <103634826+FrostPowerX@users.noreply.github.com>
2024-11-14 17:53:53 -0300

    Fix ForRelease.bat

--- Flok/scripts/ForRelease.bat
+++ Flok/scripts/ForRelease.bat
@@ -3,9 +3,9 @@
 echo %1 Solution Dir
 echo %2 Target Dir
 
-del "%~1bin\FlappyGame.pdb"
+del "%~2\Flok.pdb"
 
-xcopy "%~1res" "%~2res" /i /y /s
-tar --create --file=FlappyGame_.zip -C "%~1bin" .
+xcopy "%~1res" "%~2\res" /i /y /s
+tar --create --file=Flok_.zip -C "%~2" .
 
 pause
