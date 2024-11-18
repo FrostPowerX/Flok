479f16e5a73549d14cc3587cd177604f7e3e77b1
FrostPowerX <103634826+FrostPowerX@users.noreply.github.com>
2024-11-18 20:41:38 -0300

    Fix script ForRelease

--- Flok/scripts/ForRelease.bat
+++ Flok/scripts/ForRelease.bat
@@ -2,8 +2,9 @@
 
 echo %1 Solution Dir
 echo %2 Target Dir
+echo %3 Delete pdb
 
-del "%~2\Flok.pdb"
+if "%3~" == "1" del "%~2\Flok.pdb"
 
 xcopy "%~1res" "%~2\res" /i /y /s
 tar --create --file=Flok_.zip -C "%~2" .
